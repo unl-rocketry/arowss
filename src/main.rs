bed732f4569e8854071eb523b93c30fbd46e4cf7
yetiodo <verybluebluejay@gmail.com>
2025-03-18 01:10:50 -0500

    send structured telemetry packets and added rfd port

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::{thread::sleep, time::Duration};
+use nmea::{Nmea, SentenceType, SENTENCE_MAX_LEN};
+use serde::{Deserialize, Serialize};
+
+fn main() {
+    //TODO: need to change the ports for gps_port and rfd_port
+    let mut gps_port = serialport::new("/dev/ttyACM3", 115200)
+        .open()
+        .unwrap();
+    
+    let mut rfd_port = serialport::new("/dev/ttyACM3", 115200)
+        .open()
+        .unwrap();
+
+    let mut nmea_parser = Nmea::create_for_navigation(&[SentenceType::GGA]).unwrap();
+    let mut new_string = String::new();
+
+    loop {
+        if !gps_port.bytes_to_read().unwrap() > 83 {
+            sleep(Duration::from_millis(500));
+        }
+
+        gps_port.read_to_string(&mut new_string).unwrap_or_default();
+
+        for line in new_string.lines()
+            .filter(|l| !l.is_empty()) 
+            .filter(|l| l.len() < SENTENCE_MAX_LEN)
+        {
+            let _ = nmea_parser.parse_for_fix(&line);
+        }
+
+        let packet = TelemetryPacket {
+            latitude: nmea_parser.latitude(),
+            longitude: nmea_parser.longitude(),
+            altitude: nmea_parser.altitude(),
+            pressure: Some(0.0),
+            temperature: Some(0.0),
+        };
+
+        let packet_json = serde_json::to_string(&packet).unwrap();
+
+        rfd_port.write_all(&packet_json.as_bytes()).unwrap();
+
+        new_string.clear();
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct TelemetryPacket {
+    latitude: Option<f64>,
+    longitude: Option<f64>,
+    altitude: Option<f32>,
+    pressure: Option<f64>,
+    temperature: Option<f64>
+}
