f8ef9c04b64bf03628b31de160b0cde596959550
G2-Games <ke0bhogsg@gmail.com>
2025-04-07 14:41:17 -0500

    Started arranging functions into threads for data ccollection

--- src/bin/air_side.rs
+++ src/bin/air_side.rs
@@ -1,63 +1,71 @@
-use std::{thread::sleep, time::Duration};
+use std::{sync::Arc, thread::sleep, time::Duration};
 use nmea::{Nmea, SentenceType, SENTENCE_MAX_LEN};
 use arowss::TelemetryPacket;
+use tokio::sync::RwLock;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut rfd_port = serialport::new("/dev/ttyUSB0", 57600).open().unwrap();
     rfd_port.set_timeout(Duration::from_millis(50)).unwrap();
 
-    let (gps_tx, mut gps_rx) = tokio::sync::watch::channel(0);
-    tokio::spawn(async move { gps_loop() });
+    // Spawn GPS task
+    let gps_data = Arc::new(RwLock::new(None));
+    tokio::spawn({
+        let gps_data = gps_data.clone();
+        async move { gps_loop(gps_data.clone()) }
+    });
 
     // Main packet sending loop. A packet should be sent 4 times per second,
     // every 250ms. The packet format should allow for individual parts of
     // the packet information to be unavailable so any single part failing
     // cannot take down the whole system.
+    //
+    // Every packet is a single line of JSON, followed by a newline, followed
+    // by a CRC, followed by another newline. The CRC validates the JSON.
     loop {
         sleep(Duration::from_millis(250));
 
-        let mut packet = TelemetryPacket {
-            gps: None,
+        let gps = gps_data.try_read().map_or(None, |e| e.clone().or(None));
+
+        let packet = TelemetryPacket {
+            gps,
             pressure: None,
             temperature: None,
             voltage: None,
             current: None,
-            ..Default::default()
         };
 
-        packet.insert_crc();
-
-        dbg!(&packet);
+        let packet_crc = packet.crc();
 
         serde_json::to_writer(&mut rfd_port, &packet).unwrap();
+        rfd_port.write_all(b"\n").unwrap();
+        rfd_port.write_all(packet_crc.to_string().as_bytes()).unwrap();
         rfd_port.write_all(b"\n").unwrap();
     }
 }
 
-async fn gps_loop() -> ! {
+/// Function to read the GPS module.
+async fn gps_loop(data: Arc<RwLock<Option<Nmea>>>) -> ! {
     // Set up the GPS serial port. This must utilize the proper port on the
     // raspberry pi.
-    let mut gps_port = serialport::new("/dev/ttyACM0", 115200)
-        .open()
-        .unwrap();
+    let mut gps_port = serialport::new("/dev/ttyACM0", 115200).open().unwrap();
     gps_port.set_timeout(Duration::from_millis(50)).unwrap();
 
     // Set up and configure the NMEA parser.
     let mut nmea_parser = Nmea::create_for_navigation(&[SentenceType::GGA]).unwrap();
     let mut new_string = String::new();
 
+    // This loop should never exit!
     loop {
-        match gps_port.read_to_string(&mut new_string) {
-            Ok(_) => (),
-            Err(_) => (),
-        }
+        let _ = gps_port.read_to_string(&mut new_string);
 
         for line in new_string.lines()
             .filter(|l| !l.is_empty())
             .filter(|l| l.len() < SENTENCE_MAX_LEN)
         {
-            dbg!(&line);
             let _ = nmea_parser.parse_for_fix(&line);
         }
+
+        *data.write().await = Some(nmea_parser.clone());
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ pub const CRC_CKSUM: crc::Crc<u32> = crc::Crc::<u32>::new(&crc::CRC_32_CKSUM);
 /// about position and internal payload conditions. Most fields are optional,
 /// as it is possible for any part of the payload to be not functioning while
 /// still grabbing some data from it.
-#[derive(Debug, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
 pub struct TelemetryPacket {
     /// Full GPS telemetry information
     pub gps: Option<Nmea>,
@@ -21,22 +21,23 @@ pub struct TelemetryPacket {
 
     /// The voltage of the main battery
     pub voltage: Option<f64>,
-    /// Current being drawn by the main battery
+    /// Current being drawn by all components from the main battery
     pub current: Option<f64>,
-
-    /// CRC of the contents of this packet, not including this field, as
-    /// json.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub crc: Option<u32>,
 }
 
 impl TelemetryPacket {
-    pub fn insert_crc(&mut self) {
-        self.crc = None;
-
+    /// Calculate CRC from json serialized packet data.
+    pub fn crc(&self) -> u32 {
         let self_json = serde_json::to_string(self).unwrap();
-        let crc = CRC_CKSUM.checksum(self_json.as_bytes());
+        CRC_CKSUM.checksum(self_json.as_bytes())
+    }
 
-        self.crc = Some(crc);
+    /// Validate the packet against its CRC.
+    pub fn validate(&self, crc: u32) -> bool {
+        let self_json = serde_json::to_string(self).unwrap();
+        let new_crc = CRC_CKSUM.checksum(self_json.as_bytes());
+
+        // If they aren't equal, the data is invalid!
+        new_crc == crc
     }
 }
